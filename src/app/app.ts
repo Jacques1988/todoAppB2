30d139b4211fa4a13d3d8320515652015f819bf6
Jacques <jacquesvanluyck@gmail.com>
2026-01-23 12:41:29 +0100

    update add-new task component, responsive design, fixes #10, fixes #11, fixes #12, fixes #13

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Filter } from './filter/filter';
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Filter],
+  imports: [Header, Filter, NewTask],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
